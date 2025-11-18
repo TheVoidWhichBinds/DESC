2f60a13bc3d657f3125a77cf526330d3d0576f27
Davii Zarshenas <dz2596@nyu.edu>
2025-11-18 17:40:20 -0500

    eq_init and opt_ for logistic function

---
+++ Scratch/Runs/logistic/eq_init_fixbound.py
@@ -0,0 +1,56 @@
+#BASIC (FIXED BOUNDARY) EQUILIBRIUM MODDED WITH LOGISTIC PROFILE
+
+#Basic Equilibrium portion:
+#Import:
+import sys
+import os
+sys.path.insert(0, os.path.abspath("."))
+sys.path.append(os.path.abspath("../../../"))
+import desc.io
+from desc.continuation import solve_continuation_automatic
+from desc.equilibrium import Equilibrium
+from desc.geometry import FourierRZToroidalSurface
+from desc.profiles import PowerSeriesProfile
+from scratch.profiles.logistic import logistic,logistic_super
+
+
+
+
+#---------------------------------
+#Initializing Boundary Surface:
+surf= FourierRZToroidalSurface(
+    R_lmn=[10.0, -1.0, -0.3, 0.3],
+    modes_R=[
+        (0, 0),
+        (1, 0),
+        (1, 1),
+        (-1, -1),
+    ],  # (m,n) pairs corresponding to R_mn on previous line
+    Z_lmn=[1, -0.3, -0.3],
+    modes_Z=[(-1, 0), (-1, 1), (1, -1)],
+    NFP=19,
+)
+
+#Initializing Pressure and Iota:
+pressure = logistic_super(
+    #FINALIZE LOGISTIC_SUPER INPUTS BEFORE INPUTTING INITIAL GUESS
+)  
+iota = PowerSeriesProfile([1, 0, 1.5])  # 1 + 1.5 r^2
+
+#Constructing Equilibrium 
+eq = Equilibrium(
+    L=8,  # radial resolution
+    M=8,  # poloidal resolution
+    N=3,  # toroidal resolution
+    surface=surf,
+    pressure=pressure,
+    iota=iota,
+    Psi=1.0,  # total flux, in Webers
+)
+
+
+
+
+#----------------------------------------------------------
+#Solving Equilibrium:
+eq_init= solve_continuation_automatic(eq.copy(), verbose=3)
---
+++ Scratch/Runs/logistic/opt_fixbound.py
@@ -0,0 +1,92 @@
+#FIXED BOUNDARY QS OPTIMIZATION MODDED TO TAKE 
+#LOGISTIC FUNCTION FAMILY WEIGHTS AS OPTIMIZATION VARIABLES
+
+#Imports:
+import numpy as np
+import matplotlib.pyplot as plt
+import desc.io
+from desc.objectives import (
+    ObjectiveFunction,
+   #FixPressure, #skipping this line
+    FixIota,
+    FixPsi,
+    ForceBalance,
+    LinearObjectiveFromUser,
+)
+from desc.optimize import Optimizer
+
+
+#Loading Equilibria Family and Making a Copy of Final Iteration:
+eq_init_fixbound= desc.io.load("scratch/runs/logistic/eq_init_fixbound.h5") #initial equilibrium load - might need to generalize path
+eq_init= eq_init_fixbound[-1].copy() #copy of final eq in family
+
+
+
+
+#-----------------------------------------------------
+#Objective of Choice:
+objective= ObjectiveFunction(ForceBalance(eq=eq_init))
+
+#Importing Custom Constraint Funcs:
+from scratch.objectives.constraints import ( 
+    pressure_edge,
+    grad_pressure_axis,
+    grad_pressure_edge
+)
+
+#Constructing the Objective Wrappers for Custom Func:
+pressure_edge_zero = LinearObjectiveFromUser(
+    fun=pressure_edge,
+    thing=eq_init,
+    target=0.0,
+    weight=1.0,
+)
+grad_pressure_axis_zero = LinearObjectiveFromUser(
+    fun=grad_pressure_axis,
+    thing=eq_init,
+    target=0.0,
+    weight=1.0,
+)
+grad_pressure_edge_zero = LinearObjectiveFromUser(
+    fun=grad_pressure_edge,
+    thing=eq_init,
+    target=0.0,
+    weight=1.0,
+)
+
+#List of Constraints:
+constraints = (
+    ForceBalance(eq=eq_init),  # enforce JxB-grad(p)=0 during optimization
+   #FixPressure(eq=eq_init),  # fix pressure profile - OMITTED
+    FixIota(eq=eq_init),  # fix rotational transform profile
+    FixPsi(eq=eq_init),  # fix total toroidal magnetic flux
+)
+
+#Optimizer of Choice:
+optimizer = Optimizer("proximal-lsq-exact") #choice of optimizer
+
+
+
+
+#----------------------------------
+#Running the Optimizer:
+eq_opt, result = eq_init.optimize(
+    objective=objective,
+    constraints=constraints,
+    optimizer=optimizer,
+    ftol=5e-2,  # stopping tolerance on the function value
+    xtol=1e-6,  # stopping tolerance on the step size
+    gtol=1e-6,  # stopping tolerance on the gradient
+    maxiter=50,  # maximum number of iterations
+    options={
+        "perturb_options": {"order": 2, "verbose": 0},  # use 2nd-order perturbations
+        "solve_options": {
+            "ftol": 5e-3,
+            "xtol": 1e-6,
+            "gtol": 1e-6,
+            "verbose": 0,
+        },  # for equilibrium subproblem
+    },
+    copy=False,  # copy=False we will overwrite the eq_init object with the optimized result
+    verbose=3,
+)
