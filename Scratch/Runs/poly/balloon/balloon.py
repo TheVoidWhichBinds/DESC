d18135655cacd5323d62cbbc3a216493628290e0
Davii Zarshenas <dz2596@nyu.edu>
2026-02-24 11:51:50 -0500

    __init__.py

--- Scratch/Runs/poly/balloon/balloon.py
+++ Scratch/Runs/poly/balloon/balloon.py
@@ -18,6 +18,14 @@ from desc.objectives import (
     FixBoundaryZ,
     GenericObjective,
 )
+from scratch.objectives.poly_constraints import (
+    pressure_axis,
+    pressure_edge,
+    grad_pressure_axis,
+    grad_pressure_edge,
+    poly_monotonicity
+)
+
 
 plt.rcParams["font.size"] = 14
 sys.path.insert(0, os.path.abspath("."))
@@ -28,8 +36,6 @@ sys.path.append(os.path.abspath("../../../"))
 #-------------------------- EQUILIBRIUM SOLVE & PLOTTING --------------------------------------------------------------
 # Importing the HELIOTRON DESC equilibrium:
 eq0 = desc.examples.get("HELIOTRON")
-# Reduce resolution so it runs a bit quicker:
-eq0.change_resolution(L=12, M=6, N=2, L_grid=18, M_grid=12, N_grid=4)
 eq0.surface = eq0.get_surface_at(rho=1)
 
 
@@ -86,4 +92,97 @@ plt.xticks(fontsize=16)
 plt.yticks(fontsize=16)
 
 
-#-------------------------- EQUILIBRIUM SOLVE & PLOTTING --------------------------------------------------------------
+
+
+
+
+
+
+
+
+#-------------------------- OPTIMIZATION -----------------------------------------------------------------------------
+
+eq1 = eq0.copy() # save a copy of original for comparison
+nzetaperturn = 200 # number of point along a field line per transit
+k = 2 # determine which modes to unfix
+
+print("\n---------------------------------------")
+print(f"Optimizing boundary modes M, N <= {k}")
+print("---------------------------------------")
+
+modes_R = np.vstack(
+    (
+        [0, 0, 0],
+        eq1.surface.R_basis.modes[np.max(np.abs(eq1.surface.R_basis.modes), 1) > k, :],
+    )
+)
+modes_Z = eq1.surface.Z_basis.modes[np.max(np.abs(eq1.surface.Z_basis.modes), 1) > k, :]
+
+
+# Compiling constraints:
+constraints = (
+    ForceBalance(eq=eq1),
+    FixBoundaryR(eq=eq1, modes=modes_R),
+    FixBoundaryZ(eq=eq1, modes=modes_Z),
+    FixPressure(eq=eq1),
+    FixIota(eq=eq1),
+    FixPsi(eq=eq1),
+)
+
+
+# Grid:
+Curvature_grid = LinearGrid(
+    M=eq1.M_grid,
+    N=eq1.N_grid,
+    rho=np.array([1.0]),
+    NFP=eq1.NFP,
+    sym=eq1.sym,
+    axis=False,
+)
+
+
+# Compiling objectives:
+objective = ObjectiveFunction(
+    (
+        BallooningStability(
+            eq=eq1,
+            rho=np.array([0.8]),
+            alpha=alpha,
+            nturns=nturns,
+            nzetaperturn=nzetaperturn,
+            weight=2,
+        ),
+        AspectRatio(
+            eq=eq1,
+            bounds=(8, 11),
+            weight=1,
+        ),
+        GenericObjective(
+            f="curvature_k2_rho",
+            thing=eq1,
+            grid=Curvature_grid,
+            bounds=(-np.inf, 0),
+            weight=2,
+        ),
+    )
+)
+
+
+# Running optimizer:
+optimizer = Optimizer("proximal-lsq-exact")
+(eq1,), _ = optimizer.optimize(
+    eq1,
+    objective,
+    constraints,
+    ftol=1e-4,
+    xtol=1e-6,
+    gtol=1e-6,
+    maxiter=5, 
+    verbose=3,
+    options={"initial_trust_ratio": 2e-3},
+)
+print("Optimization complete!")
+
+
+# Plotting:
+desc.plotting.plot_comparison([eq0, eq1])
