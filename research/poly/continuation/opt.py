b3ad94773ab594397147d886a96bf08db7dc96f3
Davii Zarshenas <dz2596@nyu.edu>
2026-03-24 20:40:42 -0400

    ensured changes from ssh session didn't mess anything up

--- research/poly/continuation/opt.py
+++ research/poly/continuation/opt.py
@@ -4,7 +4,7 @@ import sys
 sys.path.append("/Users/macdaddi/DESC")
 import desc.io
 from desc.grid import LinearGrid
-from scratch.poly.poly_constraints import (
+from research.poly.poly_constraints import (
     pressure_axis,
     pressure_edge,
     grad_pressure_axis,
