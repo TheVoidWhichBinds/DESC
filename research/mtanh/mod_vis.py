b73534aa21e832dcb174a2fc285750873c6d13bf
Davii Zarshenas <dz2596@nyu.edu>
2026-03-25 17:10:20 -0400

    mtanh

--- research/mtanh/mod_vis.py
+++ research/mtanh/mod_vis.py
@@ -9,7 +9,7 @@ def mtanh_profile(x, ped, offset, sym, width, core_poly):
     zz = z / (1 + np.exp(2 * z))
     poly_part = np.polyval(poly_coeffs[::-1], zz)
 
-    y = base + poly_part * (offset - ped) / 2
+    y = base + 0*poly_part * (offset - ped) / 2
     return y
 
 
@@ -31,6 +31,6 @@ plot_mtanh(
     ped=1,
     offset=0,
     sym=0.5,
-    width=0.06,
-    core_poly=[0, 0, 0]
+    width=0.2,
+    core_poly=[0,0,0]
 )
