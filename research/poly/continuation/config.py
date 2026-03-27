cab0b9876761145688966489d4e8c24fc93a1d9e
Davii Zarshenas <dz2596@nyu.edu>
2026-03-27 10:11:08 -0400

    weights now can be chosen from config

--- research/poly/continuation/config.py
+++ research/poly/continuation/config.py
@@ -1,3 +1,5 @@
+
+#===================================================================================================================================================
 from pathlib import Path
 import os
 
@@ -44,7 +46,7 @@ def obj(use: bool, weight=None):
 
 #===================================================================================================================================================
 #============== EQUILIBRIUM INPUTS ==============#
-#-------------------------
+#=========================
 # Number of field periods:
 NFP = 10
 
@@ -65,10 +67,10 @@ L = 8
 M = 8
 N = 3
 eq_resolution = [L, M, N]
-#------------------------
+#========================
 
 
-#--------------------
+#====================
 # Grouping eq inputs:
 eq_config = {
     "NFP":           NFP,
@@ -76,22 +78,34 @@ eq_config = {
     "iota_init":     iota_init,
     "eq_resolution": eq_resolution,
 }
-#----------------------------------
+#==================================
 #=================================================#
 
 
+
+
+
+
+
+
+
+
 #============== OPTIMIZATION INPUTS ==============#
-#----------------------
+#======================
 target_aspect_ratio = 6
 ftol = 5e-3
 xtol = 1e-4
 gtol = 1e-3
 maxiter = 20
+#===========
 
+
+#====================
 optimizer_configs = [
+    #--------------------------
     {  # 1st stage optimization
-        "name": "proximal-lsq-exact",
-        "toggle_FXD": {  # fixed profiles
+        "name": "proximal-lsq-exact", # optimizer 
+        "toggle_FXD": { # fixed profiles
             # constraints
             "forcebalance_con": True,
             "fix_iota_con":     True,
@@ -99,13 +113,14 @@ optimizer_configs = [
             "fix_pressure_con": True,
 
             # objectives
-            "forcebalance_obj": obj(True, None),
-            "aspect_ratio_obj": obj(True, None),
-            "qs_obj":           obj(True, None),
-            "ballooning_obj":   obj(True, None),
-            "mercier_obj":      obj(True, None),
+            "forcebalance_obj": obj(True, 1e4),
+            "aspect_ratio_obj": obj(True, 1e0),
+            "qs_obj":           obj(True, 1e0),
+            "ballooning_obj":   obj(True, 1e0),
+            "mercier_obj":      obj(True, 1e0),
         },
-        "toggle_CON": {  # free profiles
+
+        "toggle_CON": { # free profiles
             # constraints
             "pressure_axis_con":      True,
             "pressure_edge_con":      True,
@@ -116,33 +131,36 @@ optimizer_configs = [
             "fix_psi_con":            True,
 
             # objectives
-            "forcebalance_obj": obj(True, None),
-            "aspect_ratio_obj": obj(True, None),
-            "qs_obj":           obj(True, None),
-            "ballooning_obj":   obj(True, None),
-            "mercier_obj":      obj(True, None),
-            "monotonicity_obj": obj(True, None),
+            "forcebalance_obj": obj(True, 1e4),
+            "aspect_ratio_obj": obj(True, 1e0),
+            "qs_obj":           obj(True, 1e0),
+            "ballooning_obj":   obj(True, 1e0),
+            "mercier_obj":      obj(True, 1e0),
+            "monotonicity_obj": obj(True, 1e0),
         },
     },
+    #-------------------------------------------
 
+    #--------------------------
     {  # 2nd stage optimization
-        "name": None,
-        "toggle_FXD": {
-            # constraints
+        "name": None, # optimizer
+        "toggle_FXD": { # fixed profiles
+            # Constraints:
             "forcebalance_con": False,
             "fix_iota_con":     False,
             "fix_psi_con":      False,
             "fix_pressure_con": False,
 
-            # objectives
+            # Objectives:
             "forcebalance_obj": obj(False, None),
             "aspect_ratio_obj": obj(False, None),
             "qs_obj":           obj(False, None),
             "ballooning_obj":   obj(False, None),
             "mercier_obj":      obj(False, None),
         },
-        "toggle_CON": {
-            # constraints
+
+        "toggle_CON": { # free profiles
+            # Constraints:
             "pressure_axis_con":      False,
             "pressure_edge_con":      False,
             "grad_pressure_axis_con": False,
@@ -151,7 +169,7 @@ optimizer_configs = [
             "fix_iota_con":           False,
             "fix_psi_con":            False,
 
-            # objectives
+            # Objectives:
             "forcebalance_obj": obj(False, None),
             "aspect_ratio_obj": obj(False, None),
             "qs_obj":           obj(False, None),
@@ -160,10 +178,11 @@ optimizer_configs = [
             "monotonicity_obj": obj(False, None),
         },
     },
-]
-#----------------------------------------
+]   #--------------------------------------------
+#================================================
 
-#---------------------
+
+#=====================
 # Grouping opt inputs:
 opt_config = {
     "target_aspect_ratio": target_aspect_ratio,
@@ -173,27 +192,35 @@ opt_config = {
     "maxiter":             maxiter,
     "optimizer_configs":   optimizer_configs,
 }
-#----------------------------
+#=============================================
 #=================================================#
 
 
+
+
+
+
+
+
+
+
 #================ DRIVER INPUTS ==================#
-#-------------------
+#=========================
 # Pressure maxima to test:
 p_maxima = [1e4]
 
 # Polynomial orders to test:
 n_set = [3]
-#----------
+#==========
 
 
-#------------------------
+#========================
 # Grouping driver inputs:
 driver_config = {
     "p_maxima": p_maxima,
     "n_set":    n_set,
 }
-#------------------
+#=====================
 #=================================================#
 #===================================================================================================================================================
 
