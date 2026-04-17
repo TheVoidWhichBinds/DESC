c4663df27269bd044a4e5de127a8ad2b5c5e06c0
Davii Zarshenas <dz2596@nyu.edu>
2026-04-17 00:41:22 -0400

    comparing param vs data for proximal and auglag - 4 optimizations

--- research/poly/params_vs_data/driver.py
+++ research/poly/params_vs_data/driver.py
@@ -17,7 +17,11 @@ from .helper import (
 from .opt import run_optimization
 
 
-#============== PROX_PARAMS VS. PROX_DATA VS. AUGLAG COMPARISON ====================================================================================
+
+
+
+
+#============== PROX_PARAMS VS. PROX_DATA VS. AUGLAG_PARAMS VS. AUGLAG_DATA COMPARISON ===========================================================
 def comparison(
         eq_config: dict,
         opt_config: dict,
@@ -33,7 +37,6 @@ def comparison(
     # Initializations:
     #----------------------------------------------------
     base_dir = os.path.dirname(os.path.abspath(__file__))
-    continuation_dir = os.path.join(base_dir, "continuation")
     #----------------------------------------------------
 
     #---------------------------------------------
@@ -41,10 +44,16 @@ def comparison(
     NFP = eq_config["NFP"]
     opt_toggles_all = opt_config["opt_toggles_all"]
     config_path = driver_config["config_path"]
-    #---------------------------------------------
+    #-----------------------------------------
 
-    #----------------------------------------
-    # Initializing table of objective values:
+    #------------------------------------------
+    # Generates meta-data README and directory:
+    out_dir = _next_run_dir(base_dir)
+    _write_readme(out_dir, config_path)
+    #----------------------------------
+
+    #---------------------------
+    # Table of objective values:
     column_map = [
         ("forcebalance_obj", f"Force error: "),
         ("qs", f"Quasi-symmetry (1,{NFP}) Boozer error: "),
@@ -57,12 +66,6 @@ def comparison(
     for key, label in column_map:
         if opt_toggles_all.get(key, {}).get("use", False):
             active_columns.append((key, label))
-    #----------------------------------------
-
-    #------------------------------------------
-    # Generates meta-data README and directory:
-    out_dir = _next_run_dir(continuation_dir)
-    _write_readme(out_dir, config_path)
     #------------------------------------------
     #==========================================
 
@@ -137,12 +140,12 @@ def comparison(
 
     #--------------------------
     # Saving optimized outputs:
-    opt_proxparams.save(os.path.join(out_dir, "opt_prox_params.h5"))
-    opt_proxdata.save(os.path.join(out_dir, "opt_prox_data.h5"))
-    opt_auglagparams.save(os.path.join(out_dir, "opt_auglag.h5"))
-    opt_auglagparams.save(os.path.join(out_dir, "opt_auglag.h5"))
-    #------------------------------------------------------------
-    #============================================================
+    opt_proxparams.save(os.path.join(out_dir, "opt_proxparams.h5"))
+    opt_proxdata.save(os.path.join(out_dir, "opt_proxdata.h5"))
+    opt_auglagparams.save(os.path.join(out_dir, "opt_auglagparams.h5"))
+    opt_auglagdata.save(os.path.join(out_dir, "opt_auglagdata.h5"))
+    #----------------------------------------------------------------
+    #================================================================
 
 
 
@@ -153,95 +156,111 @@ def comparison(
     rows = [
         ("Prox Params",),
         ("Prox Data",),
-        ("AugLag",),
-        ("Diff (DATA - PARAMS)",),
-        ("Diff (AUG - PARAMS)",),
+        ("AugLag Params",),
+        ("AugLag Data",),
+        ("Diff (DATA - PARAMS) Prox",),
+        ("Diff (DATA - PARAMS) AugLag",),
     ]
 
     values = []
 
-    row_prox_params = []
-    row_prox_data = []
-    row_auglag = []
-    row_diff_data_params = []
-    row_diff_auglag_params = []
+    row_proxparams = []
+    row_proxdata = []
+    row_auglagparams = []
+    row_auglagdata = []
+    row_diff_prox = []
+    row_diff_auglag = []
     #------------------------------------
 
     #-----------------------------
     # Extracting objective values:
     for key, label in active_columns:
-        fmin_prox_params, fmean_prox_params, fmax_prox_params = _safe_extract_from_result(
+        fmin_proxparams, fmean_proxparams, fmax_proxparams = _safe_extract_from_result(
             opt_result_proxparams, label
         )
-        fmin_prox_data, fmean_prox_data, fmax_prox_data = _safe_extract_from_result(
+        fmin_proxdata, fmean_proxdata, fmax_proxdata = _safe_extract_from_result(
             opt_result_proxdata, label
         )
-        fmin_auglag, fmean_auglag, fmax_auglag = _safe_extract_from_result(
-            opt_result_auglag, label
+        fmin_auglagparams, fmean_auglagparams, fmax_auglagparams = _safe_extract_from_result(
+            opt_result_auglagparams, label
+        )
+        fmin_auglagdata, fmean_auglagdata, fmax_auglagdata = _safe_extract_from_result(
+            opt_result_auglagdata, label
         )
 
-        row_prox_params.append(
-            f"f_min={sci_compact(fmin_prox_params, sig=4)}, "
-            f"f_mean={sci_compact(fmean_prox_params, sig=4)}, "
-            f"f_max={sci_compact(fmax_prox_params, sig=4)}"
+        row_proxparams.append(
+            f"f_min={sci_compact(fmin_proxparams, sig=4)}, "
+            f"f_mean={sci_compact(fmean_proxparams, sig=4)}, "
+            f"f_max={sci_compact(fmax_proxparams, sig=4)}"
         )
-        row_prox_data.append(
-            f"f_min={sci_compact(fmin_prox_data, sig=4)}, "
-            f"f_mean={sci_compact(fmean_prox_data, sig=4)}, "
-            f"f_max={sci_compact(fmax_prox_data, sig=4)}"
+        row_proxdata.append(
+            f"f_min={sci_compact(fmin_proxdata, sig=4)}, "
+            f"f_mean={sci_compact(fmean_proxdata, sig=4)}, "
+            f"f_max={sci_compact(fmax_proxdata, sig=4)}"
         )
-        row_auglag.append(
-            f"f_min={sci_compact(fmin_auglag, sig=4)}, "
-            f"f_mean={sci_compact(fmean_auglag, sig=4)}, "
-            f"f_max={sci_compact(fmax_auglag, sig=4)}"
+        row_auglagparams.append(
+            f"f_min={sci_compact(fmin_auglagparams, sig=4)}, "
+            f"f_mean={sci_compact(fmean_auglagparams, sig=4)}, "
+            f"f_max={sci_compact(fmax_auglagparams, sig=4)}"
         )
-
-        dmin_data = fmin_prox_data - fmin_prox_params
-        dmean_data = fmean_prox_data - fmean_prox_params
-        dmax_data = fmax_prox_data - fmax_prox_params
-        row_diff_data_params.append(
-            f"f_min diff={sci_compact(dmin_data, sig=4)}, "
-            f"f_mean diff={sci_compact(dmean_data, sig=4)}, "
-            f"f_max diff={sci_compact(dmax_data, sig=4)}"
+        row_auglagdata.append(
+            f"f_min={sci_compact(fmin_auglagdata, sig=4)}, "
+            f"f_mean={sci_compact(fmean_auglagdata, sig=4)}, "
+            f"f_max={sci_compact(fmax_auglagdata, sig=4)}"
         )
 
-        dmin_auglag = fmin_auglag - fmin_prox_params
-        dmean_auglag = fmean_auglag - fmean_prox_params
-        dmax_auglag = fmax_auglag - fmax_prox_params
-        row_diff_auglag_params.append(
+
+        dmin_prox = fmin_proxdata - fmin_proxparams
+        dmean_prox = fmean_proxdata - fmean_proxparams
+        dmax_prox = fmax_proxdata - fmax_proxparams
+        row_diff_prox.append(
+            f"f_min diff={sci_compact(dmin_prox, sig=4)}, "
+            f"f_mean diff={sci_compact(dmean_prox, sig=4)}, "
+            f"f_max diff={sci_compact(dmax_prox, sig=4)}"
+        )
+
+        dmin_auglag = fmin_auglagdata - fmin_auglagparams
+        dmean_auglag = fmean_auglagdata - fmean_auglagparams
+        dmax_auglag = fmax_auglagdata - fmax_auglagparams
+        row_diff_auglag.append(
             f"f_min diff={sci_compact(dmin_auglag, sig=4)}, "
             f"f_mean diff={sci_compact(dmean_auglag, sig=4)}, "
             f"f_max diff={sci_compact(dmax_auglag, sig=4)}"
         )
-    #-----------------------------------------------
+    #-----------------------------------------------------
 
     #-------------------------
     # Including Beta in table:
-    beta_prox_params = float(
-        eq_opt_prox_params.compute("<beta>_vol", override_grid=True)["<beta>_vol"]
+    beta_proxparams = float(
+        opt_proxparams.compute("<beta>_vol", override_grid=True)["<beta>_vol"]
     )
-    beta_prox_data = float(
-        eq_opt_prox_data.compute("<beta>_vol", override_grid=True)["<beta>_vol"]
+    beta_proxdata = float(
+        opt_proxdata.compute("<beta>_vol", override_grid=True)["<beta>_vol"]
     )
-    beta_auglag = float(
-        eq_opt_auglag.compute("<beta>_vol", override_grid=True)["<beta>_vol"]
+    beta_auglagparams = float(
+        opt_auglagparams.compute("<beta>_vol", override_grid=True)["<beta>_vol"]
+    )
+    beta_auglagdata = float(
+        opt_auglagdata.compute("<beta>_vol", override_grid=True)["<beta>_vol"]
     )
 
-    beta_diff_data_params = beta_prox_data - beta_prox_params
-    beta_diff_auglag_params = beta_auglag - beta_prox_params
+    beta_diff_prox = beta_proxdata - beta_proxparams
+    beta_diff_auglag = beta_auglagdata - beta_auglagparams
 
-    row_prox_params.append(f"{beta_prox_params:.4g}")
-    row_prox_data.append(f"{beta_prox_data:.4g}")
-    row_auglag.append(f"{beta_auglag:.4g}")
-    row_diff_data_params.append(f"{beta_diff_data_params:.4g}")
-    row_diff_auglag_params.append(f"{beta_diff_auglag_params:.4g}")
+    row_proxparams.append(f"{beta_proxparams:.4g}")
+    row_proxdata.append(f"{beta_proxdata:.4g}")
+    row_auglagparams.append(f"{beta_auglagparams:.4g}")
+    row_auglagdata.append(f"{beta_auglagdata:.4g}")
+    row_diff_prox.append(f"{beta_diff_prox:.4g}")
+    row_diff_auglag.append(f"{beta_diff_auglag:.4g}")
 
-    values.append(row_prox_params)
-    values.append(row_prox_data)
-    values.append(row_auglag)
-    values.append(row_diff_data_params)
-    values.append(row_diff_auglag_params)
-    #----------------------
+    values.append(row_proxparams)
+    values.append(row_proxdata)
+    values.append(row_auglagparams)
+    values.append(row_auglagdata)    
+    values.append(row_diff_prox)
+    values.append(row_diff_auglag)
+    #-----------------------------
 
     #------------------------------
     # Save table inside run folder:
@@ -276,18 +295,20 @@ def comparison(
         rho=rho,
         M=0,
         N=0,
-        NFP=eq_opt_prox_params.NFP,
-        sym=eq_opt_prox_params.sym,
+        NFP = opt_proxparams.NFP,
+        sym = opt_proxparams.sym,
     )
 
-    p_prox_params = eq_opt_prox_params.compute("p", grid=grid)["p"]
-    p_prox_data = eq_opt_prox_data.compute("p", grid=grid)["p"]
-    p_auglag = eq_opt_auglag.compute("p", grid=grid)["p"]
+    p_proxparams = opt_proxparams.compute("p", grid=grid)["p"]
+    p_proxdata = opt_proxdata.compute("p", grid=grid)["p"]
+    p_auglagparams = opt_auglagparams.compute("p", grid=grid)["p"]
+    p_auglagdata = opt_auglagdata.compute("p", grid=grid)["p"]
 
     plt.figure(figsize=(7, 5))
-    plt.plot(rho, p_prox_params, linewidth=2, label="Prox Params", color="purple")
-    plt.plot(rho, p_prox_data, linewidth=2, label="Prox Data", color="blue")
-    plt.plot(rho, p_auglag, linewidth=2, label="AugLag", color="orange")
+    plt.plot(rho, p_proxparams, linewidth=2, label="Prox Params", color="purple")
+    plt.plot(rho, p_proxdata, linewidth=2, label="Prox Data", color="blue")
+    plt.plot(rho, p_auglagparams, linewidth=2, label="AugLag Params", color="orange")
+    plt.plot(rho, p_auglagdata, linewidth=2, label="AugLag Data", color="red")
     plt.xlabel(r"$\rho$", fontsize=14)
     plt.ylabel("Pressure", fontsize=14)
     plt.title(r"Pressure vs $\rho$", fontsize=14)
@@ -306,21 +327,24 @@ def comparison(
     fig, ax = plot_comparison(
         eqs=[
             eq_init,
-            eq_opt_prox_params,
-            eq_opt_prox_data,
-            eq_opt_auglag,
+            opt_proxparams,
+            opt_proxdata,
+            opt_auglagparams,
+            opt_auglagdata,
         ],
         labels=[
             "Initial Equilibrium",
             "Optimized (_prox_params)",
             "Optimized (_prox_data)",
-            "Optimized (_auglag)",
+            "Optimized (_auglag_params)",
+            "Optimized (_auglag_data)",
         ],
         color=[
             "green",
             "purple",
             "blue",
             "orange",
+            "red",
         ],
     )
 
@@ -336,8 +360,8 @@ def comparison(
         rho=rho_grid,
         M=24,
         N=24,
-        NFP=eq_opt_prox_params.NFP,
-        sym=eq_opt_prox_params.sym,
+        NFP=opt_proxparams.NFP,
+        sym=opt_proxparams.sym,
     )
 
     def _j_parallel_profile(eq):
@@ -354,31 +378,39 @@ def comparison(
 
         return rho_unique, J_parallel_fs
 
-    rho_u_prox_params, J_parallel_prox_params = _j_parallel_profile(eq_opt_prox_params)
-    rho_u_prox_data, J_parallel_prox_data = _j_parallel_profile(eq_opt_prox_data)
-    rho_u_auglag, J_parallel_auglag = _j_parallel_profile(eq_opt_auglag)
+    rho_u_proxparams, J_parallel_proxparams = _j_parallel_profile(opt_proxparams)
+    rho_u_proxdata, J_parallel_proxdata = _j_parallel_profile(opt_proxdata)
+    rho_u_auglagparams, J_parallel_auglagparams = _j_parallel_profile(opt_auglagparams)
+    rho_u_auglagdata, J_parallel_auglagdata = _j_parallel_profile(opt_auglagdata)
 
     plt.figure(figsize=(7, 5))
     plt.plot(
-        rho_u_prox_params,
-        J_parallel_prox_params,
+        rho_u_proxparams,
+        J_parallel_proxparams,
         linewidth=2,
         label="Prox Params",
         color="purple",
     )
     plt.plot(
-        rho_u_prox_data,
-        J_parallel_prox_data,
+        rho_u_proxdata,
+        J_parallel_proxdata,
         linewidth=2,
         label="Prox Data",
         color="blue",
     )
     plt.plot(
-        rho_u_auglag,
-        J_parallel_auglag,
+        rho_u_auglagparams,
+        J_parallel_auglagparams,
         linewidth=2,
-        label="AugLag",
+        label="AugLag Params",
         color="orange",
+    )
+    plt.plot(
+        rho_u_auglagdata,
+        J_parallel_auglagdata,
+        linewidth=2,
+        label="AugLag Data",
+        color="red",
     )
     plt.xlabel(r"$\rho$", fontsize=14)
     plt.ylabel(r"$\langle J_{\parallel} \rangle$", fontsize=14)
@@ -398,18 +430,20 @@ def comparison(
         rho=rho_grid,
         M=0,
         N=0,
-        NFP=eq_opt_prox_params.NFP,
-        sym=eq_opt_prox_params.sym,
+        NFP=opt_proxparams.NFP,
+        sym=opt_proxparams.sym,
     )
 
-    iota_prox_params = eq_opt_prox_params.compute("iota", grid=grid_iota)["iota"]
-    iota_prox_data = eq_opt_prox_data.compute("iota", grid=grid_iota)["iota"]
-    iota_auglag = eq_opt_auglag.compute("iota", grid=grid_iota)["iota"]
+    iota_proxparams = opt_proxparams.compute("iota", grid=grid_iota)["iota"]
+    iota_proxdata = opt_proxdata.compute("iota", grid=grid_iota)["iota"]
+    iota_auglagparams = opt_auglagparams.compute("iota", grid=grid_iota)["iota"]
+    iota_auglagdata = opt_auglagdata.compute("iota", grid=grid_iota)["iota"]
 
     plt.figure(figsize=(7, 5))
-    plt.plot(rho_grid, iota_prox_params, linewidth=2, label="Prox Params", color="purple")
-    plt.plot(rho_grid, iota_prox_data, linewidth=2, label="Prox Data", color="blue")
-    plt.plot(rho_grid, iota_auglag, linewidth=2, label="AugLag", color="orange")
+    plt.plot(rho_grid, iota_proxparams, linewidth=2, label="Prox Params", color="purple")
+    plt.plot(rho_grid, iota_proxdata, linewidth=2, label="Prox Data", color="blue")
+    plt.plot(rho_grid, iota_auglagparams, linewidth=2, label="AugLag Params", color="orange")
+    plt.plot(rho_grid, iota_auglagdata, linewidth=2, label="AugLag Data", color="red")
     plt.xlabel(r"$\rho$", fontsize=14)
     plt.ylabel(r"$\iota$", fontsize=14)
     plt.title(r"Rotational transform vs $\rho$", fontsize=13)
