81b9ba5cd46769c26b2206134d68621217a90d35
Davii Zarshenas <dz2596@nyu.edu>
2026-05-10 22:27:07 -0400

    Final run

--- research/lit_comp/cases/driver.py
+++ research/lit_comp/cases/driver.py
@@ -83,10 +83,10 @@ except ImportError:
 
 OPTIMIZER = "lsq-auglag"
 
-FTOL = 1e-5
-XTOL = 1e-5
-GTOL = 1e-5
-CTOL = 1e-5
+FTOL = 1e-6
+XTOL = 1e-6
+GTOL = 1e-6
+CTOL = 1e-3
 
 MAXITER = 500
 MAX_NFEV = None
